8fe9c75425d6287bf88edfbcc52342a18e615905
orfnyc <ollie.r.fordham@gmail.com>
2023-12-10 12:08:46 -0500

    more setup and testing

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ const firebaseConfig = {
     measurementId: "G-PVSNCXGSB6"
   };
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
   
-  
+
 
