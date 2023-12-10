47a4f7cdde823bac7b7474cc75f7ecef7cd88856
orfnyc <ollie.r.fordham@gmail.com>
2023-12-10 10:42:36 -0500

    starting firebase setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ8iXyIxgK-H7_00BcHHlJPs8KpcU1hCE",
+    authDomain: "friendr-13d6c.firebaseapp.com",
+    projectId: "friendr-13d6c",
+    storageBucket: "friendr-13d6c.appspot.com",
+    messagingSenderId: "770859054230",
+    appId: "1:770859054230:web:d792d2624b35c6d4e93eb7",
+    measurementId: "G-PVSNCXGSB6"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
