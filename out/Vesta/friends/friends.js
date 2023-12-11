5acec0474210d43cc2463e5c9ece9141c3dd0165
Astro0250 <sidetrack920@gmail.com>
2023-12-11 13:14:01 -0500

    reformatted friends page

--- out/Vesta/friends/friends.js
+++ out/Vesta/friends/friends.js
@@ -2,18 +2,39 @@ function init() {
     let header = document.querySelector('.header');
     let friends = document.querySelector('.friends');
     let active = document.querySelector('.active-friends');
+    let friendacts = document.querySelector('.friend-actions');
 
     friends.style.left = header.offsetWidth + 'px';
+    
     active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
     active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+
+    friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+    friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
 };
 
 window.onresize = function() {
     let header = document.querySelector('.header');
     let friends = document.querySelector('.friends');
     let active = document.querySelector('.active-friends');
+    let friendacts = document.querySelector('.friend-actions');
 
     friends.style.left = header.offsetWidth + 'px';
+
     active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
     active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+
+    friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+    friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
 };
+
+function reroute(routeIndex){
+    switch(routeIndex){
+        case 1:
+            window.location.href = "find-friend/friends.html";
+        break;
+        case 2:
+            window.location.href = "friendr/friends.html";
+        break;
+    }
+}
