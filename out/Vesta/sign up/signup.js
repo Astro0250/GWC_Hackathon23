00964fab0aad3eb7ed34e338098d14355293e781
Christian <wigglytroll64@gmail.com>
2023-12-11 20:44:03 -0500

    cleaned up the sign in page

--- out/Vesta/sign up/signup.js
+++ out/Vesta/sign up/signup.js
@@ -20,29 +20,20 @@ const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider(app);
 const auth = getAuth(app);
 
-document.getElementById("googleBtn").addEventListener('click',(e) => {
-    signInWithRedirect(auth, provider);
-
-    getRedirectResult(auth)
+getRedirectResult(auth)
     .then((result) => {
         // This gives you a Google Access Token. You can use it to access Google APIs.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
 
-        // The signed-in user info.
         const user = result.user;
         // IdP data available using getAdditionalUserInfo(result)
-        // ...
 
-        alert(user.displayName);
+        window.location.replace("../home/home.html");
     }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+        console.log(error.message);
     });
+
+document.getElementById("googleBtn").addEventListener('click',(e) => {
+    signInWithRedirect(auth, provider);
   });
