6d1750fd252a1d5e481a98128e31be3ab6272944
Astro0250 <sidetrack920@gmail.com>
2023-12-13 08:34:37 -0500

    format functionallity

--- out/Vesta/interests/interestModule.js
+++ out/Vesta/interests/interestModule.js
@@ -1,24 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDocs, getDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ8iXyIxgK-H7_00BcHHlJPs8KpcU1hCE",
+    authDomain: "friendr-13d6c.firebaseapp.com",
+    databaseURL: "https://friendr-13d6c-default-rtdb.firebaseio.com",
+    projectId: "friendr-13d6c",
+    storageBucket: "friendr-13d6c.appspot.com",
+    messagingSenderId: "770859054230",
+    appId: "1:770859054230:web:d792d2624b35c6d4e93eb7",
+    measurementId: "G-PVSNCXGSB6"
+};
+
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+let currentUser = null;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = user;
+  }
+});
+
+function sendToDatabase(interestsArray) {
+    setDoc(doc(db, "users", currentUser.uid), {
+        "interests": interestsArray
+    }, {merge: true});
+}
+
 document.getElementById("submit-button").addEventListener("click", function () {
     console.log("submitting");
     submitInterests();
 });
 
+let interestsArray = [];
+
 function submitInterests() {
-    var interests = document.getElementsByClassName("Interest-Selected");
-    var interestsArray = {
-        "sports": [],
-        "music": [],
-        "movies": [],
-        "other": []
-    };
-    for (var i = 0; i < interests.length; i++) {
-        var interest = interests[i];
-        var topic = interest.parentElement.parentElement.parentElement.id;
-        interestsArray[topic].push(interest.innerHTML);
+    let interests = document.getElementsByClassName("Interest-Selected");
+    for (let i = 0; i < interests.length; i++) {
+        let interest = interests[i];
+        interestsArray.push(interest.innerHTML);
     }
     console.log(interestsArray);
     sendToDatabase(interestsArray);
-}
-function sendToDatabase(interestsArray) {
-    // :) implement this later :)
 }
