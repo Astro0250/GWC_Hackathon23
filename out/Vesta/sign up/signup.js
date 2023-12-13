460ed11ea2f2ec4fe8a992fc5dca9627baed2fa9
Christian Broomes <wigglytroll64@gmail.com>
2023-12-13 13:24:12 -0500

    testing (not done)

--- out/Vesta/sign up/signup.js
+++ out/Vesta/sign up/signup.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-//import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, getAdditionalUserInfo } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, setDoc, doc, getDoc, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
