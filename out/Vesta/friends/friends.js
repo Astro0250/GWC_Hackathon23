1e9c95df3283d581cea8b9fb7cf35e6a1b6cf05a
Christian Broomes <95944697+Rainy-Clouds@users.noreply.github.com>
2023-12-14 14:14:37 +0000

    spiced up the main pages

--- out/Vesta/friends/friends.js
+++ out/Vesta/friends/friends.js
@@ -4,29 +4,29 @@ function init() {
     let active = document.querySelector('.active-friends');
     let friendacts = document.querySelector('.friend-actions');
 
-    friends.style.left = header.offsetWidth + 'px';
+    // friends.style.left = header.offsetWidth + 'px';
     
-    active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
-    active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+    // active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+    // active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
 
-    friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
-    friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+    // friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+    // friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
 };
 
-window.onresize = function() {
-    let header = document.querySelector('.header');
-    let friends = document.querySelector('.friends');
-    let active = document.querySelector('.active-friends');
-    let friendacts = document.querySelector('.friend-actions');
+// window.onresize = function() {
+//     let header = document.querySelector('.header');
+//     let friends = document.querySelector('.friends');
+//     let active = document.querySelector('.active-friends');
+//     let friendacts = document.querySelector('.friend-actions');
 
-    friends.style.left = header.offsetWidth + 'px';
+//     friends.style.left = header.offsetWidth + 'px';
 
-    active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
-    active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+//     active.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+//     active.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
 
-    friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
-    friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
-};
+//     friendacts.style.left = header.offsetWidth + friends.offsetWidth + 'px';
+//     friendacts.style.width = window.innerWidth - header.offsetWidth - friends.offsetWidth + 'px';
+// };
 
 function reroute(routeIndex){
     switch(routeIndex){
