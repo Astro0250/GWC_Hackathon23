8317496934e789e7851c119d7943e25c10bde9e2
Christian <wigglytroll64@gmail.com>
2023-12-14 17:17:25 -0500

    Made an about page

--- out/Vesta/friends/find-friend/friends.js
+++ out/Vesta/friends/find-friend/friends.js
@@ -1,16 +1,5 @@
 function init() {
-    let header = document.querySelector('.header');
-    let orgs = document.querySelector('.find-friend');
-    orgs.style.left = header.offsetWidth + 'px';
+    // let header = document.querySelector('.header');
+    // let orgs = document.querySelector('.find-friend');
+    // orgs.style.left = header.offsetWidth + 'px';
 };
-
-window.onresize = function() {
-    let header = document.querySelector('.header');
-    let orgs = document.querySelector('.find-friend');
-    orgs.style.left = header.offsetWidth + 'px';
-};
-
-function findOrg()
-{
-
-}
