56a434a7cd9fd8eaa05d01edb5edc6a68a6cf3f6
Christian <wigglytroll64@gmail.com>
2023-12-14 17:52:07 -0500

    Finished find org CSS

--- out/Vesta/organizations/find-org/org.js
+++ out/Vesta/organizations/find-org/org.js
@@ -1,15 +1,5 @@
 function init() {
-    let header = document.querySelector('.header');
-    let orgs = document.querySelector('.find-org');
-    orgs.style.left = header.offsetWidth + 'px';
+    // let header = document.querySelector('.header');
+    // let orgs = document.querySelector('.find-org');
+    // orgs.style.left = header.offsetWidth + 'px';
 };
-
-window.onresize = function() {
-    let header = document.querySelector('.header');
-    let orgs = document.querySelector('.find-org');
-    orgs.style.left = header.offsetWidth + 'px';
-};
-
-function findOrg(){
-
-}
