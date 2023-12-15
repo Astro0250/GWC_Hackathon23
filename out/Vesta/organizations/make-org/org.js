40e517c340699ad8fe3cd414c12a892f17e61a2d
Christian <wigglytroll64@gmail.com>
2023-12-14 19:39:22 -0500

    jussy

--- out/Vesta/organizations/make-org/org.js
+++ out/Vesta/organizations/make-org/org.js
@@ -3,3 +3,14 @@ function init() {
     // let orgs = document.querySelector('.make-org');
     // orgs.style.left = header.offsetWidth + 'px';
 };
+
+function reroute(routeIndex){
+    switch(routeIndex){
+        case 1:
+            window.location.href = "org.html";
+            break;
+        case 2:
+            window.location.href = "../find-org/org.html";
+            break;
+    }
+}
